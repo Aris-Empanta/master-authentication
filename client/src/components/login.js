f199bb7b3177681fbc59283f1fd89c0428d1b6cf
Aris Empanta <eams220891@gmail.com>
2023-01-18 18:46:57 +0200

    Working on login component styling

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -3,6 +3,9 @@ import { useState } from 'react';
 import axios from 'axios'
 import { Link } from "react-router-dom";
 import { googleLogin, loginUser } from "../functions/login";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLock } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 export const Login = ({setUser, setTrainer}) => {
 
@@ -11,20 +14,35 @@ export const Login = ({setUser, setTrainer}) => {
     
 
     return(<div className="loginComponent">
-            <div id="googleLogin">
-              <button onClick={ googleLogin }>Google</button>
-            </div>
             <div id="usernamePasswordLogin">
-              <h1>Login</h1>
-              <input type="text" placeholder="username" onChange={ (e) => setUsername(e.target.value) }/>
-              <input type="password" placeholder="password"  onChange={ (e) => setPassword(e.target.value) }/>
+              <h1 id="loginTitle">User Login</h1>
+              <p className="credentialsTitles">Username</p>
+              <div className="credentialsWrapper">
+                <input type="text" placeholder="Type your username" className="credentialsInputs"
+                      onChange={ (e) => setUsername(e.target.value) }/>
+                <div className="credentialsIcon">
+                  <FontAwesomeIcon icon={ faUser } />
+                </div>
+              </div>
+              <p className="credentialsTitles">Password</p>
+              <div className="credentialsWrapper">
+                <input type="password" placeholder="Type your password" className="credentialsInputs"
+                      onChange={ (e) => setPassword(e.target.value) }/>
+                <div className="credentialsIcon">
+                  <FontAwesomeIcon icon={ faLock } />
+                </div>
+              </div>
+              <Link to='/restore-password' id="forgotPassword">Forgot your password?</Link>
               <button onClick={ () => loginUser( axios, username, 
                                                  password, setUser, 
                                                  setTrainer ) }>
-                login
-              </button>
-              <Link to='/signup'>Register</Link>
-              <Link to='/restore-password'>Forgot your password</Link>
+                 Login
+              </button>           
+              <p id="or">or</p>   
+              <div id="googleLogin">
+                <button onClick={ googleLogin } id="googleLoginButton">Login with Google</button>
+              </div>
+              <Link to='/signup' id="signupLink">Not a user? Signup </Link>
             </div>
            </div>)
 }
